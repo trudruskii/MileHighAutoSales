9159c95cdb8413a5a4fe2037b2dafb5721c69a18
drewski <aspencer22@cnm.edu>
2024-12-14 23:44:29 -0700

    blah

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <>
-          <div className="h-screen mx-auto bg-black text-white text-center border-b-2 border-gray-400 p-4">
+          <div className="h-screen mx-auto bg-white text-white border-t border-red-900 text-center border-b-2 border-gray-400 p-4">
               <h1> </h1>
           </div>
       </>
