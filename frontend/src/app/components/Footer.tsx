de0f1e2c953e934fa20b7b1a6280266a19929da9
drewski <aspencer22@cnm.edu>
2024-12-14 23:49:13 -0700

    added some disclaimer thing in footer

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Footer(){
     return(
         <>
-            <footer className="bg-gray-300 text-red-700 py-2 mt-4">
-                <div className="container mx-auto text-left">
+            <footer className="bg-gray-300 text-red-700 py-4">
+                <div className="container mx-auto text-left flex">
                     <p>Created by Spencer Tech Toolkit 2024</p>
+                    <span className="absolute right-8 font-semibold text-blue-500 hover:text-blue-700"><Link href="/">Terms and Agreements</Link></span>
+
                 </div>
             </footer>
         </>
