81d53a70b0683ea65e07e39d7c7221399f9784aa
drewski <aspencer22@cnm.edu>
2024-12-14 23:40:16 -0700

    changed navbar location and added fixed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <>
-          <div className="h-screen mx-auto bg-gradient-to-b from-gray-500 via-gray-300 to-gray-500 text-white text-center border-b-2 border-gray-400 p-4">
+          <div className="h-screen mx-auto bg-black text-white text-center border-b-2 border-gray-400 p-4">
               <h1> </h1>
           </div>
       </>
