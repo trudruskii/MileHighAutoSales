9a1d0a24bf39e99f2f4cb4463b35ad192c19bbfa
drewski <aspencer22@cnm.edu>
2024-12-14 21:01:41 -0700

    played with main page layout

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -23,28 +23,25 @@ export default function Navbarz() {
                         </p>
                 </div>
         </nav>
-
-    <nav className="bg-gray-500 border-b-2 border-l-2 border-r-2 rounded-b-lg border-red-900 p-4">
+            {/*#d7c3ac*/}
+    <nav className="bg-gradient-to-b from-[#d7c3ac] to-gray-600 border-b-2 border-l-2 border-r-2 rounded-b-lg border-red-900 p-4 shadow-xl">
         <div className="">
-            <div className="flex gap-8 text-red-500 flex justify-center">
-                <button className="text-3xl">
+            <div className="flex gap-8 text-gray-300  flex justify-end">
+                <button className="text-2xl hover:text-red-900">
                     Home
                 </button>
-                <button className="text-3xl">
+                <button className="text-2xl hover:text-red-700">
                     Inventory
                 </button>
-                <button className="text-3xl">
-                    Basket
+                <button className="text-2xl hover:text-red-700">
+                    Maintenance
                 </button>
-                <button className="text-3xl">
-                    Basket
+                <button className="text-2xl hover:text-red-700">
+                    Reviews
                 </button>
-                <button className="text-3xl">
+                <button className="text-2xl hover:text-red-700">
                     Fly high and say hi.
                 </button>
-            </div>
-            <div>
-
             </div>
         </div>
 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
       <>
-          {/*<div className="bg-black text-white text-center border-b-2 border-gray-400 p-4">*/}
-          {/*    <h1>Place Holder shit</h1>*/}
-          {/*</div>*/}
+          <div className="h-screen mx-auto bg-gradient-to-b from-gray-500 via-gray-300 to-gray-500 text-white text-center border-b-2 border-gray-400 p-4">
+              <h1> </h1>
+          </div>
       </>
 
   );
