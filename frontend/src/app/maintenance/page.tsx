d6cb78850c1fa7d1981c3a0047c79f645b0acf70
drewski <aspencer22@cnm.edu>
2024-12-18 23:33:09 -0700

    added place holder information for maintenance page

---
+++ frontend/src/app/maintenance/page.tsx
@@ -0,0 +1,37 @@
+export default function(){
+    return(
+        <>
+            <div className="flex">
+                <div className="bg-gray-800 fixed left-0 mt-1 w-92 flex flex-col gap-4 justify-center px-2">
+                    <span className="text-center">Quick Connect</span>
+                    <form>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" id="username" name="username"/>
+                    </form>
+                    <form>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" id="username" name="username"/>
+                    </form>
+                    <form>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" id="username" name="username"/>
+                    </form>
+                    <form>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" id="username" name="username"/>
+                    </form>
+                    <form>
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" id="username" name="username"/>
+                    </form>
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </>
+    )
+}
