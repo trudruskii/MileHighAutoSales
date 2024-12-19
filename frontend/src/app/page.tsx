eeae859c250df6cd91ff6daaa6b38531ebfb0131
drewski <aspencer22@cnm.edu>
2024-12-18 23:02:14 -0700

    added pictures to public dir

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
       <>
           <div className="h-screen mx-auto bg-white text-white border-t  text-center border-b-2 border-red-900 p-4">
+              <div className="flex ">
 
+              </div>
           </div>
       </>
 
