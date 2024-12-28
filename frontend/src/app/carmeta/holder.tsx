dfb1712c41be0ba89b96db432b52dd5b359dd2d2
drewski <aspencer22@cnm.edu>
2024-12-27 23:44:37 -0700

    change layout - created holder file to hold new layout for later

--- frontend/src/app/carmeta/holder.tsx
+++ frontend/src/app/carmeta/holder.tsx
@@ -24,8 +24,8 @@ import Link from "next/link";
 export default function Carmeta(){
     return(
         <>
-            <div className="absolute left-[2rem] top-[10rem] w-[37%] h-[31.5rem] border-[2px] border-black bg-gradient-to-b from-gray-100 to-gray-200 flex flex-wrap rounded-lg shadow-red-900 shadow-lg">
-                <div className="flex flex-col w-[69%] bg-white text-black px-2">
+            <div className="absolute left-[1rem] top-[2rem] w-[27%] h-[40rem] border-[2px] border-black bg-gradient-to-b from-gray-100 to-gray-200 flex flex-wrap rounded-lg shadow-red-900 shadow-lg z-30">
+                <div className="flex flex-col bg-white text-black px-2">
                     <div className="border-b-[1px] border-black py-4">
                         <span className="text-lg font-semibold">About this Vehicle</span>
                         <div className="h-[0.5px] w-[9.75rem] bg-black"></div>
@@ -40,50 +40,9 @@ export default function Carmeta(){
                     <div className="flex flex-grow mt-2 -mx-[.53rem] pt-10 bg-black ">
                     </div>
                 </div>
-                <div
-                    className="flex flex-col flex-grow justify-end items-end px-2 py-1 border-l-[1px] border-black bg-white">
-                    <span className="text-xl mr-[0.6rem] font-[tfr] text-red-900">Mile High Auto Sales</span>
-                    <span className="font-bold text-[1rem] mr-[2.4rem]">Hours of Operation</span>
-                    <div className="flex flex-row flex-grow w-[11.5rem] h-16 mb-1 pb-2">
-                        <ul className="flex flex-col gap-1 text-[.78rem]">
-                        <li>
-                                Sunday: Closed
-                            </li>
-                            <li>
-                                Monday: 8AM-5PM
-                            </li>
-                            <li>
-                                Tuesday: 8AM-5PM
-                            </li>
-                            <li>
-                                Wednesday: 8AM-5PM
-                            </li>
-                            <li>
-                                Thursday: 8AM-5PM
-                            </li>
-                            <li>
-                                Friday: 8AM-5PM
-                            </li>
-                            <li>
-                                Saturday: 10AM-3PM
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="text-sm font-[popf] text-black mr-6 py-2">
-                            <Link
-                                href="https://www.google.com/maps/place/2606+Bridge+Blvd+SW,+Albuquerque,+NM+87105/@35.0667321,-106.6981683,17z/data=!3m1!4b1!4m6!3m5!1s0x87220d138d6bef69:0xe0e80640b3dc0750!8m2!3d35.0667277!4d-106.695588!16s%2Fg%2F11cp9l52rt?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"
-                                className=" hover:text-red-900">2606 Bridge Blvd SW,<br/> Albuquerque, NM 87105</Link>
-                            <br/><Link href="tel:+15056392121" className=" hover:text-red-900">
-                            505-639-2121
-                            </Link>
-                        </div>
 
-                        <Image src={mapplaceholder} alt="Map placeholder"
-                               className="border-l-[3px] border-l-red-900 border-b-[1px] border-b-black h-[12rem] w-[12rem] rounded-l-lg rounded-b-lg mb-2"/>
-                </div>
             </div>
-            <div
-                className="flex flex-col border-[1px] border-t-[2px] border-t-red-900 border-black w-[60%] h-[35rem] bg-black rounded-md shadow-lg shadow-red-900 mx-auto mt-4 mr-4">
+            <div className="flex flex-col border-[1px] border-t-[2px] border-t-red-900 border-black w-[70%] h-[35rem] bg-black rounded-md shadow-lg shadow-red-900 mx-auto mt-4 mr-[1rem]">
                 <h3 className="text-end pt-4 mr-4 text-2xl font-extrabold text-white">2017 Kia Sorento LX Sport
                     Utility</h3>
                 <div className="flex flex-wrap justify-between">
@@ -192,12 +151,12 @@ export default function Carmeta(){
                     <div className="mt-2 relative">
                         <Image src={heroplaceholder} alt="Hero car placeholder image"
                                className="border-l-[3px] border-l-red-900 border-b-[1px] border-b-black h-[31.4rem] w-full rounded-l-lg rounded-b-lg"/>
-                            <span
-                                className="flex justify-end items-end mr-6 -mt-[30rem] font-bold text-4xl text-white font-[rrf]">$9,900</span>
-                        </div>
+                        <span
+                            className="flex justify-end items-end mr-6 -mt-[30rem] font-bold text-4xl text-white font-[rrf]">$9,900</span>
                     </div>
                 </div>
-                <div
+            </div>
+            <div
                 className="h-[40rem] bg-black border-[2px] border-b-black border-l-[1px] border-r-[1px] border-red-900 border-t-[3px] shadow-red-900 shadow-lg rounded-lg mx-4 flex flex-wrap gap-2 pt-1">
                 <Image src={stagedplaceholder1} alt="1st staged car placeholder image"
                        className="border-l-[3px] border-l-red-900 border-[1px] border-black h-[10rem] w-[10rem] rounded-l-lg rounded-b-lg ml-8"/>
@@ -230,6 +189,47 @@ export default function Carmeta(){
                        className="border-l-[3px] border-l-red-900 border-[1px] border-black h-[10rem] w-[10rem] rounded-l-lg rounded-b-lg"/>
             </div>
             <div className="spaceholder p-24">
+                <div
+                    className="flex flex-col flex-grow justify-end items-end px-2 py-1 border-l-[1px] border-black bg-white">
+                    <span className="text-xl mr-[0.6rem] font-[tfr] text-red-900">Mile High Auto Sales</span>
+                    <span className="font-bold text-[1rem] mr-[2.4rem]">Hours of Operation</span>
+                    <div className="flex flex-row flex-grow w-[11.5rem] h-16 mb-1 pb-2">
+                        <ul className="flex flex-col gap-1 text-[.78rem]">
+                            <li>
+                                Sunday: Closed
+                            </li>
+                            <li>
+                                Monday: 8AM-5PM
+                            </li>
+                            <li>
+                                Tuesday: 8AM-5PM
+                            </li>
+                            <li>
+                                Wednesday: 8AM-5PM
+                            </li>
+                            <li>
+                                Thursday: 8AM-5PM
+                            </li>
+                            <li>
+                                Friday: 8AM-5PM
+                            </li>
+                            <li>
+                                Saturday: 10AM-3PM
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="text-sm font-[popf] text-black mr-6 py-2">
+                        <Link
+                            href="https://www.google.com/maps/place/2606+Bridge+Blvd+SW,+Albuquerque,+NM+87105/@35.0667321,-106.6981683,17z/data=!3m1!4b1!4m6!3m5!1s0x87220d138d6bef69:0xe0e80640b3dc0750!8m2!3d35.0667277!4d-106.695588!16s%2Fg%2F11cp9l52rt?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"
+                            className=" hover:text-red-900">2606 Bridge Blvd SW,<br/> Albuquerque, NM 87105</Link>
+                        <br/><Link href="tel:+15056392121" className=" hover:text-red-900">
+                        505-639-2121
+                    </Link>
+                    </div>
+
+                    <Image src={mapplaceholder} alt="Map placeholder"
+                           className="border-l-[3px] border-l-red-900 border-b-[1px] border-b-black h-[12rem] w-[12rem] rounded-l-lg rounded-b-lg mb-2"/>
+                </div>
             </div>
         </>
     )
